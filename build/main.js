86e347b08db697db19c84c66a61b7b233699b458
Matt Claffey <mattclaffey@gmail.com>
2018-04-23 11:45:39 +0100

    chore: added more contect to the validation messages

--- build/main.js
+++ build/main.js
@@ -43,7 +43,7 @@
 			if(request.status === 404) {
 				const tests = activeStep.querySelector('[data-test-errors]');
 
-				tests.innerHTML = `<li class="test__item">Unable to find manifest.json</li>`;
+				tests.innerHTML = `<li class="test__item">Unable to find manifest.json please make sure it is in the root of build.</li>`;
 			}
 
 			if (request.status >= 200 && request.status < 400) {
@@ -56,17 +56,17 @@
 
 
 				const testCases = [
-					testCase(data.hasOwnProperty('short_name'), 'Manifest does not have a short name property'),
-					testCase(data.hasOwnProperty('name'), 'Manifest does not contain a name property'),
+					testCase(data.hasOwnProperty('short_name'), 'Manifest does not have a short name property. This is used if there is insufficient space.'),
+					testCase(data.hasOwnProperty('name'), 'Manifest does not contain a name property. This provides a human readblae name to the user.'),
 					testCase(data.hasOwnProperty('icons'), 'Manifest does not contain a icons property'),
-					testCase(icons.length === 7, 'You need icon sizes of 48, 72, 96, 144, 168, 192, 512 in your icons property array'),
-					testCase(data.hasOwnProperty('start_url'), 'Manifest does not contain a start_url property'),
-					testCase(data.start_url.includes('?utm_source=homescreen'), 'Start url does not have a utm source of homescreen'),
-					testCase(document.querySelector('link[rel="manifest"]') !== null, 'Manifest does not exist in the dom'),
-					testCase(data.hasOwnProperty('display'), 'Manifest does not contain a display property'),
-					testCase(data.hasOwnProperty('theme_color'), 'Manifest does not contain a theme_color property'),
-					testCase(data.hasOwnProperty('background_color'), 'Manifest does not contain a background_color property'),
-					testCase(data.hasOwnProperty('description'), 'Manifest does not contain a description property')
+					testCase(icons.length === 7, 'You need icon sizes of 48, 72, 96, 144, 168, 192, 512 in your icons property array. Check MDN for icon format.'),
+					testCase(data.hasOwnProperty('start_url'), 'Manifest does not contain a start_url property. This is the root url that the application opens.'),
+					testCase(data.start_url.includes('?utm_source=homescreen'), 'Start url does not have a utm source of homescreen. We need this to track where our users have come from.'),
+					testCase(document.querySelector('link[rel="manifest"]') !== null, 'Manifest does not exist in the dom. You need a meta tag with the rel attribute to be manifest & href to the file.'),
+					testCase(data.hasOwnProperty('display'), 'Manifest does not contain a display property. This defines the developers preferred display mode for the web application.'),
+					testCase(data.hasOwnProperty('theme_color'), 'Manifest does not contain a theme_color property. This is the colour for an application, this appears on the tab bar as well as the background colour on the splash screen.'),
+					testCase(data.hasOwnProperty('background_color'), 'Manifest does not contain a background_color. This property defines the background colour of the application.'),
+					testCase(data.hasOwnProperty('description'), 'Manifest does not contain a description property. This provides a general description of what the web application does.')
 				];
 
 				Promise.all(testCases).then(results => {
